09dfe99eb94f6a221052470ee6ba4caa5bfb2565
ye-cao <yecaomail@gmail.com>
2024-04-22 08:46:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '19a114f9-823b-4ff2-aa21-9ca615456f73';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
